dc6c9ae5352edc747fffb7cf3d6de89e755d87b2
nayoung <64996121+na3150@users.noreply.github.com>
2022-03-04 23:10:28 +0900

    Update level2행렬테두리회전하기.cpp

--- level2행렬테두리회전하기.cpp
+++ level2행렬테두리회전하기.cpp
@@ -2,28 +2,27 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 vector<vector<int>>map; 
 
-//½Ã°è¹æÇâ È¸Àü ÈÄ ÃÖ¼Ú°ª ¸®ÅÏ
+//ì‹œê³„ë°©í–¥ íšŒì „ í›„ ìµœì†Ÿê°’ ë¦¬í„´
 int rotateAndGetMin(int x1, int y1, int x2, int y2) {
     int backup = map[x1][y1];
     int Min = backup;
-    for (int i = x1 + 1; i <= x2; i++) //¿ÞÂÊ ¼¼·Î
+    for (int i = x1 + 1; i <= x2; i++) //ì™¼ìª½ ì„¸ë¡œ
     {
         map[i - 1][y1] = map[i][y1];
         Min = min(Min, map[i][y1]);
     }
-    for (int i = y1+1; i <= y2; i++) //¾Æ·¡ÂÊ °¡·Î
+    for (int i = y1+1; i <= y2; i++) //ì•„ëž˜ìª½ ê°€ë¡œ
     {
         map[x2][i - 1] = map[x2][i];
         Min = min(Min, map[x2][i]);
     }
-    for (int i = x2; i > x1; i--) { //¿À¸¥ÂÊ ¼¼·Î
+    for (int i = x2; i > x1; i--) { //ì˜¤ë¥¸ìª½ ì„¸ë¡œ
         map[i][y2] = map[i - 1][y2];
         Min = min(Min, map[i - 1][y2]);
     }
-    for (int i = y2; i > y1+1; i--) { //À§ÂÊ °¡·Î
+    for (int i = y2; i > y1+1; i--) { //ìœ„ìª½ ê°€ë¡œ
         map[x1][i] = map[x1][i - 1];
         Min = min(Min, map[x1][i - 1]);
      }
@@ -33,12 +32,12 @@ int rotateAndGetMin(int x1, int y1, int x2, int y2) {
 
 vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
     vector<int> answer;
-    //map ÃÊ±âÈ­
+    //map ì´ˆê¸°í™”
     map.assign(rows, vector<int>(columns));
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
             map[i][j] = i*columns + j+1;
-    //È¸Àü°ú ÃÖ¼Ú°ª
+    //íšŒì „ê³¼ ìµœì†Ÿê°’
     for (int i = 0; i < queries.size(); i++)
         answer.push_back(rotateAndGetMin(queries[i][0]-1, queries[i][1]-1, queries[i][2]-1, queries[i][3]-1));
     return answer;
