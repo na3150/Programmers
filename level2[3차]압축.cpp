a8040911177816c67174532f069bfd2b6f116a8c
nayoung <64996121+na3150@users.noreply.github.com>
2022-03-10 19:59:57 +0900

    level2[3차]압축

---
+++ level2[3차]압축.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, int>h;
+int cnt = 27; //저장할 색인 번호
+
+string findW(string str){
+    for (int i = 0; i < str.size(); i++)
+    {
+        string s = str.substr(0, str.size() - i);
+        if (h.count(s)) return s;
+    }
+}
+
+vector<int> solution(string msg) {
+    vector<int> answer;
+    int index = 0;
+    for (char i = 'A'; i <= 'Z'; i++) //알파벳 채우기
+    {
+        string str = "";
+        h[str+i] = i - 64;
+    }
+    while (index <= msg.size() - 1) {
+        string w = findW(msg.substr(index));
+        answer.push_back(h[w]);
+        index += w.size();
+        h[w + msg[index]] = cnt++; //w+c 사전 추가
+    }
+    return answer;
+}
